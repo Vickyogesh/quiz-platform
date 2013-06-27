df987a5f2ddfbdd9a40e72a12e10400ac4d957b1
Sergey K <skozlovf@gmail.com>
2013-06-27 20:42:07 +0700

    fixed student info

--- wsgi/quiz/service.py
+++ wsgi/quiz/service.py
@@ -59,7 +59,7 @@ def get_student_stat(user='me'):
     elif utype != 'school' and uid != stat['student']['id']:
         raise Forbidden('Forbidden.')
 
-    data = app.account.getStudent(user_id)
+    data = app.account.getStudent(user_id)['account']
     info = stat['student']
     info['name'] = data['name']
     info['surname'] = data['surname']
