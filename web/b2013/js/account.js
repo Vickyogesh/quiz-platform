c284b94a0a9c1745b9d0de3ee431e7ff5dc7901b
Sergey K <skozlovf@gmail.com>
2013-11-26 18:23:23 +0700

    Added possibility for school to edit student accounts.

--- web/b2013/js/account.js
+++ web/b2013/js/account.js
@@ -1,6 +1,10 @@
-function editAccount() {
+function editAccount(student_id) {
     var loc = window.location
     var path = loc.protocol + "//" + loc.host + loc.pathname + "?upd=1"
-    var param = '?next=' + encodeURIComponent(path);
+    var param = "?next=" + encodeURIComponent(path);
+
+    if (student_id != undefined)
+        param += "&uid=" + student_id;
+
     window.location = url("/v1/accounturl") + param;
 }
