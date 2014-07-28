ca778feb912ce91e17852719df340ae1015b450a
Sergey K <skozlovf@gmail.com>
2014-07-28 17:39:36 +0700

    Reworked js assets - backbone moved to separate bundle.

--- wsgi/quiz/ui/__init__.py
+++ wsgi/quiz/ui/__init__.py
@@ -6,12 +6,14 @@ ui = Blueprint('ui', __name__,
                template_folder='templates', static_folder='static',
                static_url_path='/static/ui')
 
-js_libs = Bundle('ui/js/libs/raphael-min.js', 'ui/js/libs/g.raphael-min.js',
+# Other filters 'yui_js', 'rjsmin'.
+js_base = Bundle('ui/js/libs/raphael-min.js', 'ui/js/libs/g.raphael-min.js',
                  'ui/js/libs/g.line.js', 'ui/js/libs/g.pie.js',
-                 'ui/js/libs/json2.js', 'ui/js/libs/underscore-min.js',
-                 'ui/js/libs/backbone-min.js',
-                 # filters='yui_js',
-                 filters='rjsmin', output='ui/gen/libs.js')
+                 filters='jsmin', output='ui/gen/base_libs.js')
+
+js_bb = Bundle('ui/js/libs/json2.js', 'ui/js/libs/underscore-min.js',
+               'ui/js/libs/backbone-min.js',
+               filters='jsmin', output='ui/gen/bb.js')
 
 js_ui = Bundle('ui/js/chart.js', 'ui/js/expressbar.js', 'ui/js/userstat.js',
                filters='jsmin', output='ui/gen/ui.js')
@@ -20,7 +22,8 @@ css_ui = Bundle('ui/css/style.css', 'ui/css/startup.css',
                 'ui/css/menu.css', 'ui/css/statistics.css',
                 filters='cssmin', output='ui/gen/ui.css')
 
-assets.register('libs.js', js_libs)
+assets.register('base_libs.js', js_base)
+assets.register('bb.js', js_bb)
 assets.register('ui.js', js_ui)
 assets.register('ui.css', css_ui)
 
