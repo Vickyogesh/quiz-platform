7296136d0307904cef0b4dd08da8362a2df7dc69
Sergey K <skozlovf@gmail.com>
2014-09-18 17:22:53 +0700

    Added test view.

--- wsgi/quiz/ui/__init__.py
+++ wsgi/quiz/ui/__init__.py
@@ -68,3 +68,8 @@ from . import client
 school.register_views(ui)
 client.register_views(ui)
 
+# TODO: remove me, only for testing apache URL routing
+@ui.route('/img/<name>')
+def img_test(name):
+    print "WSGI HANDING [/img/%s]" % name
+    return "ok %s" % name
