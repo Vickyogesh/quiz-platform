65d9788852c908a2d0d6b06c7e7e6fcee383cfff
Sergey K <skozlovf@gmail.com>
2014-10-15 13:08:11 +0700

    Added endpoint for the facebook tabs.

--- wsgi/quiz/ui/__init__.py
+++ wsgi/quiz/ui/__init__.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, abort, Response, url_for
 from flask.ext.assets import Bundle
 from .. import assets
+from ..login import QUIZ_ID_MAP
 
 #NOTE: g.line.js has some changes/fixes.
 ui = Blueprint('ui', __name__,
@@ -72,3 +73,18 @@ client.register_views(ui)
 @ui.route('/policy')
 def policy():
     return util.render_template('ui/policy.html')
+
+
+@ui.route('/<quiz_fullname>/aggiungi-piattaforma-a-pagina-facebook')
+def fb_tab(quiz_fullname):
+    if quiz_fullname not in QUIZ_ID_MAP:
+        abort(404)
+    tmpl = """<!DOCTYPE html>
+    <html>
+    <body>
+    <script>top.location = "http://www.facebook.com/dialog/pagetab?app_id=306969962800273&redirect_uri={0}"</script>
+    </body>
+    </html>
+    """.format(url_for('.index', quiz_fullname=quiz_fullname, fblogin=1,
+                       _external=True))
+    return Response(tmpl, mimetype='text/html')
