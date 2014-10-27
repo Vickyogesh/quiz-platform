5f3aaaa2579bbc156ca8581b89fdcbbaab9b3e43
Sergey K <skozlovf@gmail.com>
2014-10-27 16:55:06 +0600

    Updated fullscreen endpoint.

--- wsgi/quiz/ui/fullscreen.py
+++ wsgi/quiz/ui/fullscreen.py
@@ -4,10 +4,8 @@ from .util import render_template, check_access
 from .. import access
 
 
-@ui.route('/<type>/fmenu')
+@ui.route('/c/fmenu')
 @check_access
 @access.be_client_or_guest.require()
-def fullscreen(type):
-    if type != 'c' and type != 's':
-        abort(404)
-    return render_template('ui/fullscreen_wrapper.html', acc_type=type)
+def fullscreen():
+    return render_template('ui/fullscreen_wrapper.html', acc_type='c')
