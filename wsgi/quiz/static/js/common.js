f82ab590ebcaa11fc29bd98fb3563474f50e541c
Sergey K <skozlovf@gmail.com>
2015-02-28 11:10:55 +0600

    Added fallback Funcion.bind() implementation.

--- wsgi/quiz/static/js/common.js
+++ wsgi/quiz/static/js/common.js
@@ -1,4 +1,29 @@
 (function() {
+
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== "function") {
+                throw new TypeError(
+                    "Function.prototype.bind - what is trying to be bound is not callable");
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP    = function() {},
+                fBound  = function() {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                            ? this
+                            : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
+
     // NOTE: busy element id is hardcoded.
     var busy_el_id = "#busy_layer";
 
