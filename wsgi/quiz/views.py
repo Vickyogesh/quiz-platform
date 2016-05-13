41078e69b1673cee2239cb0edeb9d55ebd04c724
Sergey Kozlov <skozlovf@gmail.com>
2016-05-13 09:22:52 +0600

    Make default quizB 2013

--- wsgi/quiz/views.py
+++ wsgi/quiz/views.py
@@ -2,9 +2,9 @@ from flask import render_template
 from . import app, quiz_b, quiz_cqc, quiz_am, quiz_cde
 
 quiz_b.quiz.init_app(app, quiz_id=1, quiz_year=2011, base_prefix='/ui')
-quiz_b.quiz.init_app(app, quiz_id=3, quiz_year=2013, base_prefix='/ui')
-quiz_b.quiz.init_app(app, quiz_id=50, quiz_year=2016, base_prefix='/ui',
+quiz_b.quiz.init_app(app, quiz_id=3, quiz_year=2013, base_prefix='/ui',
                      no_url_year=True, main=True)
+quiz_b.quiz.init_app(app, quiz_id=50, quiz_year=2016, base_prefix='/ui')
 
 quiz_cqc.quiz.init_app(app, quiz_id=2, quiz_year=2011, base_prefix='/ui',
                        no_url_year=True)
