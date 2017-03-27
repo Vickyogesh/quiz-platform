ece237f1fb81683efea7bf51555087c9e7bee4fa
vertuzz <vertuzz@yandex.ru>
2017-03-27 10:12:36 +0300

    Fixed stat update for revisioni

--- wsgi/quiz/stat.py
+++ wsgi/quiz/stat.py
@@ -22,6 +22,8 @@ def fill_exam_stat(stat, result, range_key, quiz_type_names):
             names = ['C1-C1E', 'C1-C1E_97', 'C-CE', 'C-CE_C1', 'D1-D1E', 'D-DE',
                      'D-DE_D1']
             quiz_name = 'cde %s' % names[row.quiz_type - 5]
+        elif 60 <= row.quiz_type <= 66:
+            quiz_name = quiz_type_names[60]
         else:
             quiz_name = quiz_type_names[row.quiz_type]
 
