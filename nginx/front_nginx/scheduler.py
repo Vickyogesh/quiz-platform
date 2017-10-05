85301537f82438763ddbeb76f458be34458a351f
vertuzz <vertuzz@yandex.ru>
2017-10-05 10:15:57 +0300

    Added logrotate to front proxy

--- nginx/front_nginx/scheduler.py
+++ nginx/front_nginx/scheduler.py
@@ -4,8 +4,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 def cert_renew():
     os.system('certbot renew')
 
+def log_rotate():
+    os.system('logrotate -f /etc/logrotate.d/nginx')
+
 scheduler = BlockingScheduler()
 scheduler.add_job(cert_renew, 'cron', hour=2)
+scheduler.add_job(log_rotate, 'interval', hours=12)
 
 if __name__ == '__main__':
     scheduler.start()
