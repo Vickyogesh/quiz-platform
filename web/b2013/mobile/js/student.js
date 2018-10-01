88ef3455fc095924d83c68ac74d9fce704d125e4
iammashed <rustamallakov>
2018-10-01 18:41:12 +0500

    testing scope 'public_profile'

--- web/b2013/mobile/js/student.js
+++ web/b2013/mobile/js/student.js
@@ -61,7 +61,7 @@ $("#page-student").bind("pageinit", function() {
               // The person is not logged into Facebook, do nothing.
               console.log("not login");
           }
-      }, {scope: 'publish_to_groups'});
+      }, {scope: 'public_profile'});
   });
 
   $("#page-student").bind('pageshow', function() {
